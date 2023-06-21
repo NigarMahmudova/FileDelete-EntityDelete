ceae1146a8b7075e636784504f8bd70e91937f97
NigarMahmudova <nigarem@code.edu.az>
2023-06-20 23:31:18 -0700

    first commit

---
+++ PustokBookStore/PustokBookStore/Attributes/CustomValidationAttributes/AllowedTypes.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PustokBookStore.Attributes.CustomValidationAttributes
+{
+    public class AllowedTypes:ValidationAttribute
+    {
+        private readonly string[] _types;
+        public AllowedTypes(params string[] types)
+        {
+            _types = types;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+
+            if (value is IFormFile)
+            {
+                var type = (IFormFile)value;
+
+                foreach (var item in _types)
+                {
+                    if (type.ToString() != item)
+                    {
+                        return new ValidationResult($"ImageFile must be {item}");
+                    }
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
